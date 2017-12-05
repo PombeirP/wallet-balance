51e1f57ad49b1597bdd0ad710f44dee0aa760f7e
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2017-12-05 23:43:23 +0100

    Implement workers and jobs

--- main.go
+++ main.go
@@ -9,39 +9,63 @@ import (
 func main() {
 	fmt.Println("Fetching balances...")
 
-	client := &http.Client{Timeout: 10 * time.Second}
-
 	// Load balance checkers for each crypto-currency
 	balanceCheckers := loadConfigFromJSON()
 
-	// Kick off asynchronous balance check for each crypto-currency
-	done := make(chan *CryptoBalanceChecker)
-	for _, checker := range balanceCheckers {
-		go checker.GetAddressBalances(client, done)
-	}
+	done := make(chan bool)
+	go fetchBalances(balanceCheckers, 3, done)
 
-	// Wait for result and check for errors
+	// Calculate max symbol length for formatting
 	var maxSymbolLength int
-	for index := 0; index < len(balanceCheckers); index++ {
-		completedCheck := <-done
-		if completedCheck.Error != nil {
-			fmt.Println(completedCheck.Error)
-			return
-		}
-
-		if length := len(completedCheck.Symbol); length > maxSymbolLength {
+	for _, checker := range balanceCheckers {
+		if length := len(checker.Symbol); length > maxSymbolLength {
 			maxSymbolLength = length
 		}
 	}
 
+	<-done
+
 	// Print out balances
 	totalUsdBalance := 0.
 	for _, checker := range balanceCheckers {
-		format := fmt.Sprintf("%s balance: %%%df %%%ds (in USD: %%7.2f$, 1%%%ds = %%.2f$)\n", checker.Symbol, 13-len(checker.Symbol), -maxSymbolLength, -maxSymbolLength)
-		usdBalance := checker.Balance * checker.UsdExchangeRate
-		totalUsdBalance += usdBalance
-		fmt.Printf(format, checker.Balance, checker.Symbol, usdBalance, checker.Symbol, checker.UsdExchangeRate)
+		if checker.Error != nil {
+			fmt.Printf("%s: %s\n", checker.Symbol, checker.Error)
+		} else {
+			format := fmt.Sprintf("%s balance: %%%df %%%ds (in USD: %%7.2f$, 1%%%ds = %%.2f$)\n", checker.Symbol, 13-len(checker.Symbol), -maxSymbolLength, -maxSymbolLength)
+			usdBalance := checker.Balance * checker.UsdExchangeRate
+			totalUsdBalance += usdBalance
+			fmt.Printf(format, checker.Balance, checker.Symbol, usdBalance, checker.Symbol, checker.UsdExchangeRate)
+		}
 	}
 	fmt.Println("------------------------------------------")
 	fmt.Printf("USD balance: %.2f$\n", totalUsdBalance)
+}
+
+func fetchBalances(balanceCheckers []*CryptoBalanceChecker, workerCount int, done chan<- bool) {
+	client := &http.Client{Timeout: 10 * time.Second}
+	jobs := make(chan *CryptoBalanceChecker, workerCount)
+	results := make(chan *CryptoBalanceChecker, len(balanceCheckers))
+
+	// Start up some workers
+	worker := func(jobs <-chan *CryptoBalanceChecker, results chan<- *CryptoBalanceChecker) {
+		for j := range jobs {
+			j.GetAddressBalances(client, results)
+		}
+	}
+	for index := 0; index < cap(jobs); index++ {
+		go worker(jobs, results)
+	}
+
+	// Kick off a job for each crypto-currency check
+	for _, checker := range balanceCheckers {
+		jobs <- checker
+	}
+	close(jobs)
+
+	// Wait for results
+	for _ = range balanceCheckers {
+		<-results
+	}
+
+	done <- true
 }
